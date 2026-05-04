18a43c9735d2e25aa4147e8d5c6d33a88f5faaef
marian <elias-marian@web.de>
2026-05-04 20:34:55 +0200

    Remove static remotes

--- app/module-federation.config.ts
+++ app/module-federation.config.ts
@@ -2,9 +2,6 @@ import { createModuleFederationConfig } from '@module-federation/modern-js-v3';
 
 export default createModuleFederationConfig({
   name: 'orca',
-  remotes: {
-    provider: 'rslib_provider@http://localhost:3001/mf-manifest.json',
-  },
   shared: {
     react: { singleton: true },
     'react-dom': { singleton: true },
