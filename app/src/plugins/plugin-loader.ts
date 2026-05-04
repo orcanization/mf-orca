4eac2f92e8e7a85d61749d79727f9aba36a0b289
marian <elias-marian@web.de>
2026-05-04 18:59:30 +0200

    Add remote plugin loading

--- app/src/plugins/plugin-loader.ts
+++ app/src/plugins/plugin-loader.ts
@@ -1,8 +1,9 @@
 import { apiClient } from '@/api/api-utils';
 import type { Plugin } from '@/api/models';
 import { getInstance } from '@module-federation/runtime';
+import type { RuntimePlugin, RuntimePluginModule } from '@orca/sdk';
 
-async function loadRemotePlugin(plugin: Plugin) {
+async function loadRemotePlugin(plugin: Plugin): Promise<RuntimePlugin> {
   const entryUrl = apiClient().getPluginUrl(plugin);
 
   const mf = getInstance();
@@ -17,5 +18,12 @@ async function loadRemotePlugin(plugin: Plugin) {
       entry: entryUrl,
     },
   ]);
-  // const pluginModule = await mf.loadRemote<>(plugin.id.toString())
+  const pluginModule = await mf.loadRemote<RuntimePluginModule>(
+    plugin.id.toString(),
+  );
+  if (!pluginModule) {
+    throw new Error(`Failed to load plugin module for plugin "${plugin.name}"`);
+  }
+
+  return pluginModule.default;
 }
