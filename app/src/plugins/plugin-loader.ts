42bb0cc332c2c865b5ec9bc26cf03d337c6e8483
marian <elias-marian@web.de>
2026-05-09 19:36:50 +0200

    Fix plugin loading

--- app/src/plugins/plugin-loader.ts
+++ app/src/plugins/plugin-loader.ts
@@ -1,29 +1,18 @@
 import { apiClient } from '@/api/api-utils';
 import type { Plugin } from '@/api/models';
-import { getInstance } from '@module-federation/runtime';
-import type { RuntimePlugin, RuntimePluginModule } from '@orca/sdk';
+import type { ModuleFederation } from '@module-federation/runtime';
 
-export async function loadRemotePlugin(plugin: Plugin): Promise<RuntimePlugin> {
-  const entryUrl = apiClient().getPluginUrl(plugin);
-
-  const mf = getInstance();
-  if (!mf) {
-    throw new Error(`Failed to create mf instance for plugin "${plugin.name}"`);
-  }
-
-  mf.registerRemotes([
-    {
+export function loadRemotePlugins(
+  mfInstance: ModuleFederation,
+  plugins: Plugin[],
+) {
+  const remotes = plugins.map(plugin => {
+    const entryUrl = apiClient().getPluginUrl(plugin);
+    return {
       name: plugin.id.toString(),
       alias: plugin.name,
       entry: entryUrl,
-    },
-  ]);
-  const pluginModule = await mf.loadRemote<RuntimePluginModule>(
-    plugin.id.toString(),
-  );
-  if (!pluginModule) {
-    throw new Error(`Failed to load plugin module for plugin "${plugin.name}"`);
-  }
-
-  return pluginModule.default;
+    };
+  });
+  mfInstance.registerRemotes(remotes);
 }
--- app/src/routes/page.tsx
+++ app/src/routes/page.tsx
@@ -1,8 +1,9 @@
 import { Helmet } from '@modern-js/runtime/head';
 import './index.css';
-import { loadRemotePlugin } from '@/plugins/plugin-loader';
+import { loadRemotePlugins } from '@/plugins/plugin-loader';
 import type { PageData } from '@/routes/page.data';
 import { useLoaderData } from '@modern-js/runtime/router';
+import { getInstance, registerRemotes } from '@module-federation/runtime';
 import type { RuntimePlugin } from '@orca/sdk';
 import { useEffect, useState } from 'react';
 
@@ -14,14 +15,26 @@ export default () => {
 
   useEffect(() => {
     if (plugins.length > 0) {
-      loadRemotePlugin(plugins[0])
-        .then(plugin => {
-          setLoadedPlugin(plugin);
-        })
-        .catch(err => {
-          setError(err.message);
-          console.error('Failed to load plugin:', err);
-        });
+      window.__ORCA_API_URL__ = 'http://localhost:8888';
+
+      const mfInstance = getInstance();
+      if (!mfInstance)
+        throw new Error('Module Federation instance is not initialized');
+
+      loadRemotePlugins(mfInstance, plugins);
+
+      for (const plugin of plugins) {
+        mfInstance
+          .loadRemote(plugin.id.toString())
+          .then(module => {
+            const typedModule = module as { default: RuntimePlugin };
+            setLoadedPlugin(typedModule.default);
+          })
+          .catch(err => {
+            setError(err.message);
+            console.error('Failed to load plugin:', err);
+          });
+      }
     }
   }, [plugins]);
 
