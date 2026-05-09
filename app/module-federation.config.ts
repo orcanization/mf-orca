6b4c41e2538809f472d2ff7c0560b03452663804
marian <elias-marian@web.de>
2026-05-09 21:19:26 +0200

    Bump react versions to 19 to fix version conflict with host app

--- app/module-federation.config.ts
+++ app/module-federation.config.ts
@@ -3,7 +3,29 @@ import { createModuleFederationConfig } from '@module-federation/modern-js-v3';
 export default createModuleFederationConfig({
   name: 'orca',
   shared: {
-    react: { singleton: true },
-    'react-dom': { singleton: true },
+    react: {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: '^19.0.0',
+      eager: true,
+    },
+    'react-dom': {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: '^19.0.0',
+      eager: true,
+    },
+    'react/jsx-runtime': {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: '^19.0.0',
+      eager: true,
+    },
+    'react/jsx-dev-runtime': {
+      singleton: true,
+      strictVersion: false,
+      requiredVersion: '^19.0.0',
+      eager: true,
+    },
   },
 });
