bf3551e6105704d6d680c385032ece08a600111b
marian <elias-marian@web.de>
2026-05-09 21:27:46 +0200

    Revert changes to simplify code. It's enough to fix the react version in the provider code

--- app/module-federation.config.ts
+++ app/module-federation.config.ts
@@ -3,29 +3,7 @@ import { createModuleFederationConfig } from '@module-federation/modern-js-v3';
 export default createModuleFederationConfig({
   name: 'orca',
   shared: {
-    react: {
-      singleton: true,
-      strictVersion: false,
-      requiredVersion: '^19.0.0',
-      eager: true,
-    },
-    'react-dom': {
-      singleton: true,
-      strictVersion: false,
-      requiredVersion: '^19.0.0',
-      eager: true,
-    },
-    'react/jsx-runtime': {
-      singleton: true,
-      strictVersion: false,
-      requiredVersion: '^19.0.0',
-      eager: true,
-    },
-    'react/jsx-dev-runtime': {
-      singleton: true,
-      strictVersion: false,
-      requiredVersion: '^19.0.0',
-      eager: true,
-    },
+    react: { singleton: true },
+    'react-dom': { singleton: true },
   },
 });
